13cb6b399d3ec0957ce04a20a824876de3a05d82
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-10-28 17:06:57 +0000

    Package.json/server.js
    Added the infrastructure/blueprint for the server.js to ensure it is able to run, and changed servers package.json to ensure it works to run and has nodemon working perfectly.

--- server/server.js
+++ server/server.js
@@ -2,3 +2,19 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import pkg from 'pg'
+
+dotenv.config();
+const { Pool } = pkg;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+
+
+const PORT = 5000 || PORT;
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
