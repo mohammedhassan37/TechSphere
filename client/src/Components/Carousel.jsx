629485916f48533ea11d532d3c86b6520c37c16e
Reduan-khan <prantakhan13@gmail.com>
2025-10-31 12:07:00 +0000

    Add image carousel component and styles
    Introduced a Carousel component with three image slides and basic auto-sliding functionality using inline script. Added corresponding CSS for layout, transitions, and image styling.

--- client/src/Components/Carousel.jsx
+++ client/src/Components/Carousel.jsx
@@ -2,8 +2,43 @@ import '../Styles/Carousel.css';
 
 function Carousel(){
     return(
-        <>
-            
+        
+            <div className="slider-container">
+                <div class="slide">
+                    <img src="17pro.png" alt="Slide 1"/>
+                </div>
+                <div class="slide">
+                    <img src="17.png" alt="Slide 2"/>
+                </div>
+                <div class="slide">
+                    <img src="slide3.jpg" alt="Slide 3"/>
+                </div>
+
+
+
+                <script>
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  const slides = document.getElementsByClassName("slide");
+
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+
+  slideIndex++;
+  if (slideIndex > slides.length) { slideIndex = 1 }
+
+
+  slides[slideIndex - 1].style.display = "block";
+
+
+  setTimeout(showSlides, 3000);
+}
+</script>
         </>
     )
 }
