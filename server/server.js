414c2444eb6befb8c6e48f6975401d2563ad3ec4
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-11-25 11:50:18 +0000

    Add user signup endpoint with bcrypt password hashing
    Introduces a new /signup POST endpoint to register users. Validates input, checks for existing email, hashes passwords using bcrypt, and stores user credentials securely in the database.

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import pkg from 'pg'
+import bcrypt from 'bcrypt';
 
 dotenv.config();
 const { Pool } = pkg;
@@ -14,6 +15,42 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+//SIGNUP
+app.post('/signup', async (req,res) => {
+    try{
+        const {email,password,confirmPassword} = req.body;
+
+        if(!email || !password || !confirmPassword){
+            return res.status(400).json({success:false,message:"Fields cannot be empty"})
+        }
+
+        if(password != confirmPassword){
+            return res.status(400).json({success:false,message:"Passwords do not match"})
+        }
+
+        const [existingUser] =  await db.query(
+        `SELECT * FROM users WHERE email = ?`,
+        [email]
+        );
+
+        if (existingUser.length > 0){
+        return res.status(400).json({success:false,message:"Email already registered."})
+        }
+
+        const hashedPassword = await bcrypt.hash(password,10);
+
+        await db.query(
+        "INSERT INTO users (email, password) VALUES (?,?)" ,
+        [email,hashedPassword]
+    );
+
+    return res.status(201).json({success:true,message:"Email has been registered."})
+
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({success:false,message:"Server error"})
+    }
+})
 
 
 
