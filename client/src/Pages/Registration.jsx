7dcffae08e5b8578311328c142e2edbb1a8ce5b2
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-11-25 16:25:44 +0000

    Update Registration.jsx
    test

--- client/src/Pages/Registration.jsx
+++ client/src/Pages/Registration.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
+
 function Registration(){
+    
+    const [isLogin, setIsLogin] = useState(true);
+
     return(
         <>
-            <h1>yo</h1>
+            
+            <input type="text" placeholder="Enter your email" />
+
         </>
     )
 }
