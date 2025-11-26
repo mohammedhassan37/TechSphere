36a0c5843b2241a8d006023a1885da45af21bdc7
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-11-26 08:13:33 +0000

    testing
    more testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,8 +9,8 @@ import Tablets from './Pages/Tablets.jsx';
 import TV from './Pages/TV.jsx';
 import Registration from "./Pages/Registration.jsx";
 
-import Carousel from './Components/Carousel.jsx'
-import Header from './Components/Header.jsx'
+import Carousel from './components-temp/Carousel.jsx'
+import Header from './components-temp/Header.jsx'
 
 function App() {
   return (
