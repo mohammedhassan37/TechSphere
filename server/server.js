c12d084fb91792a5fbbfb51e45dd19bee412a483
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-11-26 06:40:55 +0000

    Deploy changes test
    test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import pkg from 'pg'
+import pkg from 'pg';
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const { Pool } = pkg;
@@ -10,11 +12,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Database
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+});
 
+// Static frontend serving
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+});
 
-
-const PORT = 5000 || PORT;
-
+// Start
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
