e474bf3f137b5ea72ec80277d076811ad9794686
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-11-26 07:56:49 +0000

    render deployment
    Fixed importing for render deployment

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header.jsx";
+
 
 import About from './Pages/About.jsx'
 import Headphones from './Pages/Headphones.jsx';
@@ -9,7 +9,8 @@ import Tablets from './Pages/Tablets.jsx';
 import TV from './Pages/TV.jsx';
 import Registration from "./Pages/Registration.jsx";
 
-import Carousel from "./components/Carousel.jsx";
+import Header from "./Components/Header.jsx";
+import Carousel from "./Components/Carousel.jsx";
 
 function App() {
   return (
