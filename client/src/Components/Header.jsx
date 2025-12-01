0cbe9d6918e77a3601349ce23f357974f5a9dac7
mohammedhassan37 <mohammedhassan37@outlook.com>
2025-12-01 12:43:24 +0000

    Add logo image to header and update navigation
    added header changes for subhan

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,6 +1,8 @@
 import '../Styles/Header.css'
 import { Link } from "react-router-dom";
 import basket from '../assets/basket.png';
+import logo from '../assets/logo.png'
+
 
 function Header() {
     return (
@@ -8,21 +10,21 @@ function Header() {
         <div class="nav-container">
         <header>
             <nav>
-                <div class="logo">
-                    
-                    <Link to="/" id='title'><h1></h1></Link>
+                <div className="logo">
+                     <img src={logo}
+                        alt="logo"
+                        style={{ height: "70px", width: "70px", cursor: "pointer"}} 
+                        onClick={()=> window.location.href = "http://localhost:5173/"}
+                     />
                 </div>
-                
-                <div class="nav-links">
+
+                <div className ="nav-links">
                     <Link to="/phone">Phone</Link>
                     <Link to="/Tablets">Tablets</Link>
                     <Link to="/Headphones">Headphones</Link>
                     <Link to="/TV">TV & Home</Link>
                     <Link to="/Smartwatch">Smartwatch</Link>
                     <Link to="/About">About</Link>
-                    <Link to="/Contact">Contact</Link>
-                    {/* Registration is temporary */}
-                    <Link to="/Registration">Register</Link>
                 </div>
                 
                 <div class="nav-search">
