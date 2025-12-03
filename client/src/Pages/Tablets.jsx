773307eeb5a0125f8a5299874e48167ac1b8fef5
Maryam Salem <faye2003.saad@outlook.com>
2025-12-03 20:29:43 +0000

    Refactor Tablets page to use React conventions
    Replaced all 'class' attributes with 'className' for JSX compatibility and imported Link from react-router-dom. Updated 'Add to Basket' buttons for the first two products to use the Link component instead of anchor tags.

--- client/src/Pages/Tablets.jsx
+++ client/src/Pages/Tablets.jsx
@@ -1,45 +1,49 @@
+import { Link } from "react-router-dom";
+
+
+
 function Tablets(){
     return(
         <>
             
-<div class="product_container">
+<div className="product_container">
 
 
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="tablet.webp" alt="Product 1"/>
         <h3>Samsung Galaxy Tab S11 Ultra 14.6in 256GB Wi-Fi Tablet</h3>
         <p>£999.00</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Samsung Galaxy Tab S11 Ultra 14.6in 256GB Wi-Fi Tablet"
            data-price="999.00"
            data-image="tablet.webp">
            Add to Basket
-        </a>
+        </Link>
     </div>
 
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="honour.webp" alt="Product 2"/>
         <h3>HONOR Pad X8a 11 Inch 128GB Wi-Fi Tablet</h3>
         <p>$109.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="HONOR Pad X8a 11 Inch 128GB Wi-Fi Tablet"
            data-price="109.00"
            data-image="honour.webp">
            Add to Basket
-        </a>
+        </Link>
 
         
     </div>
 
 
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="lenevo.webp" alt="Product 3"/>
         <h3>Lenovo Idea Tab 11 Inch 128GB Wi-Fi Tablet</h3>
         <p>$149.99</p>
 
-        <a href="#" class="add-btn"
+        <a href="#" className="add-btn"
            data-name="Lenovo Idea Tab 11 Inch 128GB Wi-Fi Tablet"
            data-price="149.99"
            data-image="lenevo.webp">
@@ -48,12 +52,12 @@ function Tablets(){
     </div>
 
     
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="tablet_A9.webp" alt="Product 4"/>
         <h3>Samsung Galaxy Tab A9+ 11in 64GB Wi-Fi Tablet</h3>
         <p>$209.99</p>
 
-        <a href="#" class="add-btn"
+        <a href="#" className="add-btn"
            data-name="Samsung Galaxy Tab A9+ 11in 64GB Wi-Fi Tablet"
            data-price="209.99"
            data-image="tablet_A9.webp">
@@ -62,12 +66,12 @@ function Tablets(){
     </div>
 
    
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="ipad.webp" alt="Product 5"/>
         <h3>Apple iPad Pro 2024 11 Inch Wi-Fi 256GB</h3>
         <p>$899.99</p>
 
-        <a href="#" class="add-btn"
+        <a href="#" className="add-btn"
            data-name="Apple iPad Pro 2024 11 Inch Wi-Fi 256GB"
            data-price="899.99"
            data-image="ipad.webp">
@@ -76,12 +80,12 @@ function Tablets(){
     </div>
 
    
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="ipad_air.webp" alt="Product 5"/>
         <h3>Apple iPad Air 2025 13 Inch Wi-Fi 256GB</h3>
         <p>$799.99</p>
 
-        <a href="#" class="add-btn"
+        <a href="#" className="add-btn"
            data-name="Apple iPad Air 2025 13 Inch Wi-Fi 256GB"
            data-price="799.99"
            data-image="ipad_air.webp">
@@ -90,12 +94,12 @@ function Tablets(){
     </div>
 
    
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="amazon_fire.webp" alt="Product 5"/>
         <h3>Amazon Fire Max 11 Inch 64GB Wi-Fi Tablet</h3>
         <p>$129.99</p>
 
-        <a href="#" class="add-btn"
+        <a href="#" className="add-btn"
            data-name="Amazon Fire Max 11 Inch 64GB Wi-Fi Tablet"
            data-price="129.99"
            data-image="amazon_fire.webp">
@@ -104,12 +108,12 @@ function Tablets(){
     </div>
 
     
-    <div class="product_cards">
+    <div className="product_cards">
         <img src="amzon_fire2.webp" alt="Product 5"/>
         <h3>Amazon Fire HD 10 2024 10.1 Inch 32GB Wi-Fi Tablet</h3>
         <p>$89.99</p>
 
-        <a href="#" class="add-btn"
+        <a href="#" className="add-btn"
            data-name="Amazon Fire HD 10 2024 10.1 Inch 32GB Wi-Fi Tablet"
            data-price="89.99"
            data-image="amzon_fire2.webp">
