bced5ad30929cf7791a839d5c38ca40adbaff790
Maryam Salem <faye2003.saad@outlook.com>
2025-12-03 20:36:33 +0000

    Refactor image sources to use imported variables
    Updated all tablet product image src and data-image attributes to use imported variables instead of string literals. This improves maintainability and ensures correct image references throughout the Tablets page.

--- client/src/Pages/Tablets.jsx
+++ client/src/Pages/Tablets.jsx
@@ -10,27 +10,27 @@ function Tablets(){
 
 
     <div className="product_cards">
-        <img src="tablet.webp" alt="Product 1"/>
+        <img src={tablet} alt="Product 1"/>
         <h3>Samsung Galaxy Tab S11 Ultra 14.6in 256GB Wi-Fi Tablet</h3>
         <p>£999.00</p>
 
         <Link href="#" className="add-btn"
            data-name="Samsung Galaxy Tab S11 Ultra 14.6in 256GB Wi-Fi Tablet"
            data-price="999.00"
-           data-image="tablet.webp">
+           data-image={tablet}>
            Add to Basket
         </Link>
     </div>
 
     <div className="product_cards">
-        <img src="honour.webp" alt="Product 2"/>
+        <img src={honour} alt="Product 2"/>
         <h3>HONOR Pad X8a 11 Inch 128GB Wi-Fi Tablet</h3>
         <p>$109.99</p>
 
         <Link href="#" className="add-btn"
            data-name="HONOR Pad X8a 11 Inch 128GB Wi-Fi Tablet"
            data-price="109.00"
-           data-image="honour.webp">
+           data-image={honour}>
            Add to Basket
         </Link>
 
@@ -39,7 +39,7 @@ function Tablets(){
 
 
     <div className="product_cards">
-        <img src="lenevo.webp" alt="Product 3"/>
+        <img src={lenevo} alt="Product 3"/>
         <h3>Lenovo Idea Tab 11 Inch 128GB Wi-Fi Tablet</h3>
         <p>$149.99</p>
 
@@ -53,21 +53,21 @@ function Tablets(){
 
     
     <div className="product_cards">
-        <img src="tablet_A9.webp" alt="Product 4"/>
+        <img src={tablet} alt="Product 4"/>
         <h3>Samsung Galaxy Tab A9+ 11in 64GB Wi-Fi Tablet</h3>
         <p>$209.99</p>
 
         <a href="#" className="add-btn"
            data-name="Samsung Galaxy Tab A9+ 11in 64GB Wi-Fi Tablet"
            data-price="209.99"
-           data-image="tablet_A9.webp">
+           data-image={tablet}>
            Add to Basket
         </a>
     </div>
 
    
     <div className="product_cards">
-        <img src="ipad.webp" alt="Product 5"/>
+        <img src={ipad} alt="Product 5"/>
         <h3>Apple iPad Pro 2024 11 Inch Wi-Fi 256GB</h3>
         <p>$899.99</p>
 
@@ -81,21 +81,21 @@ function Tablets(){
 
    
     <div className="product_cards">
-        <img src="ipad_air.webp" alt="Product 5"/>
+        <img src={ipad} alt="Product 5"/>
         <h3>Apple iPad Air 2025 13 Inch Wi-Fi 256GB</h3>
         <p>$799.99</p>
 
         <a href="#" className="add-btn"
            data-name="Apple iPad Air 2025 13 Inch Wi-Fi 256GB"
            data-price="799.99"
-           data-image="ipad_air.webp">
+           data-image={ipad}>
            Add to Basket
         </a>
     </div>
 
    
     <div className="product_cards">
-        <img src="amazon_fire.webp" alt="Product 5"/>
+        <img src={amazon_fire1} alt="Product 5"/>
         <h3>Amazon Fire Max 11 Inch 64GB Wi-Fi Tablet</h3>
         <p>$129.99</p>
 
@@ -109,7 +109,7 @@ function Tablets(){
 
     
     <div className="product_cards">
-        <img src="amzon_fire2.webp" alt="Product 5"/>
+        <img src={amazon_fire2} alt="Product 5"/>
         <h3>Amazon Fire HD 10 2024 10.1 Inch 32GB Wi-Fi Tablet</h3>
         <p>$89.99</p>
 
