6c817f281d8abccbdfcd49626189bd5cc3061573
Maryam Salem <faye2003.saad@outlook.com>
2025-12-04 13:49:06 +0000

    Refactor Smartwatch page to use JSX conventions
    Replaced HTML class attributes with className and updated <a> tags to <Link> components for proper React usage. Also fixed self-closing <img> tags and ensured consistent JSX formatting throughout the Smartwatch product listing.

--- client/src/Pages/Smartwatch.jsx
+++ client/src/Pages/Smartwatch.jsx
@@ -13,117 +13,117 @@ function Smartwatch(){
     return(
         <>
     
-<div class="product_container">
+<div className="product_container">
 
    
-    <div class="product_cards">
-        <img src={smartwatch} alt="Product 1">
+    <div className="product_cards">
+        <img src={smartwatch} alt="Product 1"/>
         <h3>HUAWEI Watch Fit 3 Smart Watch-Grey</h3>
         <p>$139.00</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" class="add-btn"
            data-name="HUAWEI Watch Fit 3 Smart Watch"
            data-price="139.00"
            data-image={smartwatch}>
            Add to Basket
-        </a>
+        </Link>
     </div>
 
-    <div class="product_cards">
-        <img src={reflex} alt="Product 2">
+    <div className="product_cards">
+        <img src={reflex} alt="Product 2"/>
         <h3>Reflex Active Black Smart Watch-Black</h3>
         <p>$35.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Reflex Active Black Smart Watch"
            data-price="35.99"
            data-image={reflex_watch}>
            Add to Basket
-           </a>
+           </Link>
     </div>
 
     
-    <div class="product_cards">
-        <img src={apple_watch} alt="Product 3">
+    <div className="product_cards">
+        <img src={apple_watch} alt="Product 3"/>
         <h3>Apple Watch Ultra 3 Black Ocean Band</h3>
         <p>$719.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Apple Watch Ultra 3 Black Ocean Band"
            data-price="719.99"
-           data-image="apple_watch.webp">
+           data-image={apple_watch}>
            Add to Basket
-           </a>
+           </Link>
     </div>
 
     
-    <div class="product_cards">
-        <img src={samsung_watch} alt="Product 4">
+    <div className="product_cards">
+        <img src={samsung_watch} alt="Product 4"/>
         <h3>Samsung Galaxy Watch7 40mm Smart Watch</h3>
         <p>$249.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Samsung Galaxy Watch7 40mm Smart Watch"
            data-price="249.99"
            data-image={samsung_watch}>
            Add to Basket
-           </a>
+           </Link>
     </div>
 
    
-    <div class="product_cards">
-        <img src={garmin_watch} alt="Product 5">
+    <div className="product_cards">
+        <img src={garmin_watch} alt="Product 5"/>
         <h3>Garmin Forerunner 165 GPS Running Smart Watch</h3>
         <p>$169.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Garmin Forerunner 165 GPS Running Smart Watch"
            data-price="169.99"
            data-image={garmin_watch}>
            Add to Basket
-           </a>
+           </Link>
     </div>
 
     
-    <div class="product_cards">
-        <img src={fitbit_watch} alt="Product 5">
+    <div className="product_cards">
+        <img src={fitbit_watch} alt="Product 5"/>
         <h3>Fitbit Versa 4 Smart Watch - Waterfall Blue</h3>
         <p>$129.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Fitbit Versa 4 Smart Watch - Waterfall Blue"
            data-price="129.99"
            data-image={fitbit_watch}>
            Add to Basket
-           </a>
+           </Link>
     </div>
 
-    <!-- Product 7 -->
-    <div class="product_cards">
-        <img src={samsung_watch2} alt="Product 5">
+    
+    <div className="product_cards">
+        <img src={samsung_watch2} alt="Product 5"/>
         <h3>Samsung Galaxy Watch8 40mm Smart Watch</h3>
         <p>$249.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Samsung Galaxy Watch8 40mm Smart Watch"
            data-price="249.99"
            data-image={samsung_watch2}>
            Add to Basket
-           </a>
+           </Link>
     </div>
 
     
-    <div class="product_cards">
-        <img src={garmin_watch2} alt="Product 5">
+    <div className="product_cards">
+        <img src={garmin_watch2} alt="Product 5"/>
         <h3>Garmin Fenix 8 47mm Smart Watch - Glass Grey & Black</h3>
         <p>$599.99</p>
 
-        <a href="#" class="add-btn"
+        <Link href="#" className="add-btn"
            data-name="Garmin Fenix 8 47mm Smart Watch - Glass Grey & Black"
            data-price="599.99"
            data-image={garmin_watch2}>
            Add to Basket
-           </a>
+           </Link>
     </div>
 
 </div>
