a52fd4c021a77f292c320599afba359e64fe0195
mowarsaston <200179485@aston.ac.uk>
2025-12-05 00:21:08 +0000

    Update GoogleMaps.jsx

--- client/src/Components/GoogleMaps.jsx
+++ client/src/Components/GoogleMaps.jsx
@@ -2,8 +2,8 @@ export default function GoogleMap() {
   return (
     <div style={{ width: "100%", height: "450px", marginTop: "20px" }}>
       <iframe
-        title="Google Map Location"
-        src="https://www.google.com/maps/embed?pb=YOUR_EMBED_URL"
+        title="Our Location"
+        src="https://www.google.com/maps/embed?pb=https://www.google.com/maps/place/Bullring/@52.4776478,-1.8969896,17z/data=!3m2!4b1!5s0x4870bc88ee0ffd5b:0x7edf0a3a313a015b!4m6!3m5!1s0x4870bc88b0a23981:0x2f48a9bcc2e96950!8m2!3d52.4776446!4d-1.8944147!16zL20vMDE4eWR6?entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D"
         width="100%"
         height="100%"
         style={{ 
