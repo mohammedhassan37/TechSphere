8641af1ce7bcf52575277acd3bf1a4800e79b32c
7subby <niazilittlemaster322@gmail.com>
2025-12-05 00:59:21 +0000

    MAP

--- client/src/Pages/Contact.jsx
+++ client/src/Pages/Contact.jsx
@@ -50,6 +50,8 @@ function Contact() {
 
 
   return (
+
+    
     <div className="ContactMainDiv">
       <form className="formcontact" onSubmit={hSubmission}>
         <h2>Contact Us!</h2>
@@ -93,10 +95,25 @@ function Contact() {
 
         <button type="submit">Send Inquiry</button>
       </form>
+
+    <div className="Location">
+        <iframe
+          title="Location"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2430.0821742748612!2d-1.8969896239522979!3d52.47764783936515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bc88b0a23981%3A0x2f48a9bcc2e96950!2sBullring!5e0!3m2!1sen!2suk!4v1764894183198!5m2!1sen!2suk&zoom=17"
+          width="450"
+          height="450"
+          style={{
+            border: 0,
+            borderRadius: "10px",
+          }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </div>
     </div>
-
-
   );
 }
+
 
 export default Contact;
