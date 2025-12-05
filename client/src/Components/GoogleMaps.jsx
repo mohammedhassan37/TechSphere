73c38375283c28e669f867839744ae6acf5b1ac9
mowarsaston <200179485@aston.ac.uk>
2025-12-05 00:24:02 +0000

    Update GoogleMaps.jsx

--- client/src/Components/GoogleMaps.jsx
+++ client/src/Components/GoogleMaps.jsx
@@ -3,7 +3,7 @@ export default function GoogleMap() {
     <div style={{ width: "100%", height: "450px", marginTop: "20px" }}>
       <iframe
         title="Our Location"
-        src="https://www.google.com/maps/embed?pb=https://www.google.com/maps/place/Bullring/@52.4776478,-1.8969896,17z/data=!3m2!4b1!5s0x4870bc88ee0ffd5b:0x7edf0a3a313a015b!4m6!3m5!1s0x4870bc88b0a23981:0x2f48a9bcc2e96950!8m2!3d52.4776446!4d-1.8944147!16zL20vMDE4eWR6?entry=ttu&g_ep=EgoyMDI1MTIwMi4wIKXMDSoASAFQAw%3D%3D"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2430.0821742748612!2d-1.8969896239522979!3d52.47764783936515!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4870bc88b0a23981%3A0x2f48a9bcc2e96950!2sBullring!5e0!3m2!1sen!2suk!4v1764894183198!5m2!1sen!2suk" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>"
         width="100%"
         height="100%"
         style={{ 
