7d380f627fb8bbabed67f459035d6f8dc2c1fbec
mohammedhassan37 <mohammedhassan37@outlook.com>
2026-03-04 18:43:55 +0000

    initial structure

--- client/src/Pages/AccountDetails.jsx
+++ client/src/Pages/AccountDetails.jsx
@@ -1,7 +1,13 @@
 function AccountDetails(){
     return(
         <>
-            <h1>yo</h1>
+            <div className="AccountDetailsIntro">
+                <h1>Account Details</h1>
+                <p>Mange your profile and preferences.</p>
+            </div>
+            <div className="AccountDetailsMain">
+                
+            </div>
         </>
     )
 }
