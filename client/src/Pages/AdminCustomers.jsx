476d61d159b9f3045ae7263adfa846aa3ec06bad
Kieran53 <240106146@aston.ac.uk>
2026-03-17 15:35:47 +0000

    Updated AdminCustomers

--- client/src/Pages/AdminCustomers.jsx
+++ client/src/Pages/AdminCustomers.jsx
@@ -5,16 +5,16 @@ function AdminCustomers() {
       <div className="admin-customers">
         <div className="admin-customers-header">
           <h1>Manage Customers</h1>
-          
       </div>
-      
+
       <p className="admin-customers-info">
         View and manage customers:
       </p>
+
           <div className="orders-features">
           <input
             type="text"
-            placeholder="Search by Customer Name or Email..." className="search"/>
+            placeholder="Search by Customer Name or Email.." className="search"/>
             </div>
 
       <div className="customers-container">
@@ -33,75 +33,75 @@ function AdminCustomers() {
 
           <tbody>
             <tr>
-              <td>Name1</td>
-              <td>Name123@gmail.com</td>
+              <td>Alice</td>
+              <td>alice123@gmail.com</td>
               <td>07794892293</td>
               <td>Birmingham</td>
               <td>4</td>
               <td>£999.99</td>
               <td>
-                <button className="edit-button">Edit Customer</button>
-                <button className="delete-button">Delete Customer</button>
+                <button className="edit-button">Edit Row</button>
+                <button className="delete-button">Delete Row</button>
               </td>
             </tr>
             <tr>
-              <td>Name2</td>
-              <td>Name2@outlook.com</td>  
+              <td>Bob</td>
+              <td>bob2@outlook.com</td>  
               <td>07754178357</td>
               <td>London</td>
               <td>2</td>
               <td>£301.98</td>
               <td>
-                <button className="edit-button">Edit Customer</button>
-                <button className="delete-button">Delete Customer</button>
+                <button className="edit-button">Edit Row</button>
+                <button className="delete-button">Delete Row</button>
               </td>
             </tr>
             <tr>
-              <td>Name3</td>
-              <td>Name2@outlook.com</td>
+              <td>Charlie</td>
+              <td>charlie213@outlook.com</td>
               <td>07355825797</td>
               <td>Birmingham</td>
               <td>1</td>
               <td>£1099.99</td>
               <td>
-                <button className="edit-button">Edit Customer</button>
-                <button className="delete-button">Delete Customer</button>
+                <button className="edit-button">Edit Row</button>
+                <button className="delete-button">Delete Row</button>
               </td>
             </tr>
             <tr>
-              <td>Name4</td>
-              <td>Name4123@gmail.com</td>
+              <td>John</td>
+              <td>john31@gmail.com</td>
               <td>07355825797</td>
               <td>Manchester</td>
               <td>2</td>
               <td>£149.99</td>
               <td>
-                <button className="edit-button">Edit Customer</button>
-                <button className="delete-button">Delete Customer</button>
+                <button className="edit-button">Edit Row</button>
+                <button className="delete-button">Delete Row</button>
               </td>
             </tr>
             <tr>
-              <td>Name5</td>
-              <td>Name5@outlook.com</td>
+              <td>Jane</td>
+              <td>jane87@outlook.com</td>
               <td>07275921650</td>
               <td>Glasgow</td>
               <td>1</td>
               <td>£229.99</td>
               <td>
-                <button className="edit-button">Edit Customer</button>
-                <button className="delete-button">Delete Customer</button>
+                <button className="edit-button">Edit Row</button>
+                <button className="delete-button">Delete Row</button>
               </td>
             </tr>
             <tr>
-              <td>Name6</td>
-              <td>Name6@gmail.com</td>
+              <td>Henry</td>
+              <td>henry6@yahoo.com</td>
               <td>0748928492</td>
               <td>Cardiff</td>
               <td>3</td>
               <td>£599.97</td>
               <td>
-                <button className="edit-button">Edit Customer</button>
-                <button className="delete-button">Delete Customer</button>
+                <button className="edit-button">Edit Row</button>
+                <button className="delete-button">Delete Row</button>
               </td>
             </tr>
           </tbody>
