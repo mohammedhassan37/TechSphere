5069fb3630120748e651ff09a891969d9ab10aed
Reduan-khan <prantakhan13@gmail.com>
2026-03-20 02:12:43 +0000

    Update ShopByCategory.jsx

--- client/src/components/ShopByCategory.jsx
+++ client/src/components/ShopByCategory.jsx
@@ -20,7 +20,26 @@ function ShopByCategory() {
           <h3>Phones</h3>
           <p>Latest smartphones and accessories</p>
         </Link>
-        
+        <Link to="/Headphones" className="category-card">
+          <img src={Headphones} alt="Headphones" />
+          <h3>Headphones</h3>
+            <p>High-quality audio devices</p>
+        </Link>
+        <Link to="/smartwatch" className="category-card">
+          <img src={Smartwatch} alt="Smartwatches" />
+          <h3>Smartwatches</h3>
+            <p>Stylish and functional smartwatches</p>
+        </Link>
+        <Link to="/tablets" className="category-card">
+          <img src={Tablet} alt="Tablets" />
+            <h3>Tablets</h3>
+            <p>Portable and powerful tablets</p>
+        </Link>
+        <Link to="/tv" className="category-card">
+          <img src={TV} alt="TVs" />
+            <h3>TVs</h3>
+            <p>Immersive entertainment experience</p>
+        </Link>
         </div>
        
             </section>
