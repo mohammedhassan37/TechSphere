e20e3e6b615dddbf044741e5350a39b9d14a8a92
Reduan-khan <prantakhan13@gmail.com>
2026-03-22 13:50:08 +0000

    Making the chatbot button functional

--- client/src/components/ChatBot.jsx
+++ client/src/components/ChatBot.jsx
@@ -1,22 +1,43 @@
 import "../Styles/ChatBot.css";
+import { useState } from "react";
 
 function ChatBot() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleChat = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="chatbot-wrapper">
-      <button className="chat-button">
+      <button className="chat-button" onClick={() => toggleChat()}>
         💬
       </button>
 
-       <div className="chatbot-box" id="chatbotBox">
+      {isOpen && (
+      <div className="chatbot-box" id="chatbotBox">
         <div className="chatbot-header">
           <div>
             <h3>TechSpere Support</h3>
             <p>How can we assist you?</p>
           </div>
+          <div className="chatbot-messages" id="chatMessages">
+          <div className="message bot">
+            Hi 👋 Welcome to TechSpere Support. How can I help you today?
+          </div>
+        </div>
+        <div className="chatbot-input-area">
+          <input
+            type="text"
+            id="userInput"
+            placeholder="Type your message..."/>
+          <button id="sendButton">Send</button>
 
+        </div>
+        </div>
       </div>
-      </div>
-      </div>
+      )}
+    </div>
   );
 }
 
